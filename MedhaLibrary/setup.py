c4fe52c414c42da91a39ed18c840d47fa2e17602
nikk97t <NikhilTanni@users.noreply.github.com>
2022-04-17 19:15:13 +0530

    version variable fix while importing library: #14 - Bug in installation of Medha library

--- MedhaLibrary/setup.py
+++ MedhaLibrary/setup.py
@@ -14,13 +14,17 @@ if (os.path.exists(distdir)):
 
 # timestampted version suffix
 build_datetime = datetime.datetime.now()
-version = '2.0.1'
+version = '2.0.3'
 print("version: {}".format(version))
 
 # write build details to version.txt
-with open(os.path.join("src", "medhalib", "version.txt"), "w") as vfl:
-    details = str("""Version: {version} [Date: {date}]""").format(version=version, date=build_datetime)
-    vfl.write(details)
+with open(os.path.join("src", "medhalib", "__init__plh.py"), "r") as vflr:
+  filedt = vflr.read()
+  details = str("""Version: {version} [Date: {date}]""").format(version=version, date=build_datetime)
+  filedt = filedt.replace("$VERSION_INFO_PLACEHOLDER", details)
+
+  with open(os.path.join("src", "medhalib", "__init__.py"), "w") as vflw:
+    vflw.write(filedt)
 
 setup(
   name = 'medhalib',
@@ -31,7 +35,7 @@ setup(
   packages=find_packages('src', exclude="tests"),
   package_dir={'': 'src'},
   data_files=[
-    ('', ["src/medhalib/version.txt"])
+    # ('', ["src/medhalib/version.txt"])
   ],
   include_package_data=True,
   tests_require = [
--- MedhaLibrary/src/medhalib/__init__plh.py
+++ MedhaLibrary/src/medhalib/__init__plh.py
@@ -3,7 +3,7 @@ import os.path
 
 # read version details from version.txt
 def read_version():
-    version_file = """Version: 2.0.3 [Date: 2022-04-17 19:09:03.258586]"""
+    version_file = """$VERSION_INFO_PLACEHOLDER"""
     return version_file
 
 __version__=read_version()
